1330c5f2657d83e6c8c14333a11f57e1e2cf2cf2
Shift7 <luky.hubert@gmail.com>
2026-06-17 14:09:17 +0200

    Updated CodeGen to be more modular.

--- qlc/CodeGen.cpp
+++ qlc/CodeGen.cpp
@@ -1,6 +1,51 @@
 #include "CodeGen.hpp"
 #include <iostream>
 
+void CodeGen::compileFunction(FuncNode *node) {
+
+    // defines function type (i32) todo: more function types
+    llvm::Type* i32Type = llvm::Type::getInt32Ty(*context);
+    llvm::FunctionType* funcType = llvm::FunctionType::get(i32Type, {}, false); // return i32, doesnt take arguments
+
+    // creates the function
+    llvm::Function* llvmFunc = llvm::Function::Create(
+        funcType,
+        llvm::Function::ExternalLinkage, // External means the OS can see it to run it
+        node->identifier,            // "main"
+        module.get()                     // The blueprint it belongs to
+    );
+
+    // creates entry block
+    llvm::BasicBlock* entryBlock = llvm::BasicBlock::Create(*context, "entry", llvmFunc);
+
+    builder->SetInsertPoint(entryBlock); // moves to cursor into enteryblock.
+
+    // loop through the body and pass every statement to our router
+    for (const auto& statement : node->body) { // ?
+        compileLiteral(statement.get());
+    }
+}
+
+llvm::Value* CodeGen::compileLiteral(Node* node) {
+    if (!node) return nullptr;
+
+    switch (node->type) {
+        case NodeType::RetInt:
+            return compileReturn(static_cast<RetIntNode*>(node)); // ?
+
+            // todo: add future features here
+
+        default:
+            std::cerr << "Unknown node type in CodeGen!" << std::endl;
+            return nullptr;
+    }
+}
+
+llvm::Value* CodeGen::compileReturn(RetIntNode* node) {
+    llvm::Value* retVal = llvm::ConstantInt::get(*context, llvm::APInt(32, node->value, true));
+    return builder->CreateRet(retVal);
+}
+
 CodeGen::CodeGen() {
     context = std::make_unique<llvm::LLVMContext>();
     module = std::make_unique<llvm::Module>("RunQ-Lang_Module", *context);
@@ -10,38 +55,8 @@ CodeGen::CodeGen() {
 void CodeGen::generate(Ast &ast) {
     if (!ast.root) return;
 
-    auto* funcNode = static_cast<FuncNode*>(ast.root.get()); // grabs the root node.
-
-    // defines function type (i32)
-    llvm::Type* i32Type = llvm::Type::getInt32Ty(*context);
-    llvm::FunctionType* funcType = llvm::FunctionType::get(i32Type, {}, false); // return i32, doesnt take arguments
-
-    // creates the function
-    llvm::Function* llvmFunc = llvm::Function::Create(
-        funcType,
-        llvm::Function::ExternalLinkage, // External means the OS can see it to run it
-        funcNode->identifier,            // "main"
-        module.get()                     // The blueprint it belongs to
-    );
-
-    // creates entry block
-    llvm::BasicBlock* entryBlock = llvm::BasicBlock::Create(*context, "entry", llvmFunc);
-
-    builder->SetInsertPoint(entryBlock); // moves to cursor into enteryblock.
-
-    // translates the body
-    for (const auto& statement : funcNode->body) {
-
-        if (statement->type == NodeType::RetInt) { // its a return statement
-            auto* retNode = static_cast<RetIntNode*>(statement.get());
-
-            // converts the value into an LLVM Arbitrary Precision Integer (APInt).
-            // 32 = bit width, retNode->value = the number 67, true = it is signed.
-            llvm::Value* retVal = llvm::ConstantInt::get(*context, llvm::APInt(32, retNode->value, true));
-
-            // writes ret i32 67
-            builder->CreateRet(retVal);
-        }
+    if (ast.root->type == NodeType::Func) {
+        compileFunction(static_cast<FuncNode*>(ast.root.get()));
     }
 
     std::cout << "\n--- LLVM IR Output ---\n";
--- qlc/CodeGen.hpp
+++ qlc/CodeGen.hpp
@@ -12,6 +12,10 @@ private:
     std::unique_ptr<llvm::Module> module;
     std::unique_ptr<llvm::IRBuilder<>> builder;
 
+    void compileFunction(FuncNode* node); // todo: split fn and endpoint function
+    llvm::Value *compileLiteral(Node *node);
+    llvm::Value *compileReturn(RetIntNode *node);
+
 public:
     CodeGen();
 
